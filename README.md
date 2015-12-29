c3ee507342d5bd09221de9980603a97ef1937b3c
Adam Tache <at201@duke.edu>
2015-12-29 02:45:48 -0500

    Refactored JS code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# personal-website
+Adam Tache's personal website. Made from scratch using Javascript, jQuery, HTML, and CSS.
+
+HTML file: index.html, JS file: custom.js, CSS file: custom.css
+
+Type animation uses typed.js and hover animations using hover.js
--- js/custom.js
+++ js/custom.js
@@ -1,76 +1,65 @@
 $(document).ready(function(){
 	$("#background").fadeTo(2000, 0.5);
+	var topSlides = ["#nav","#title","#content"];
+	var leftSlides = ["#side"];
+	var topLength = topSlides.length;
+	var leftLength = leftSlides.length;
+	var del = 500;
+	var x;
+	for(x=0; x<topLength; x++){
+		topSlide(topSlides[x], del+=125);
+	}
+	for(x=0; x<leftLength; x++){
+		leftSlide(leftSlides[x], del+=125);
+	}
 
-	$("#nav").delay(500).fadeIn({queue: false, duration: 500});
-	$("#nav").delay(500).animate({"top": "0px"}, 500);
+	function topSlide(ele, del){
+		$(ele).delay(del).fadeIn({queue: false, duration: 500});
+		$(ele).delay(del).animate({"top": "0px"}, 500);
+	}
 
-	$("#title").delay(500).fadeIn({queue: false, duration: 500});
-	$("#title").delay(750).animate({"top": "0px"}, 500);
+	function leftSlide(ele, del){
+		$(ele).delay(del).fadeIn({queue: false, duration: 500});
+		$(ele).delay(del).animate({"left": "0px"}, 500);
+	}
 
-	$("#content").delay(875).fadeIn({queue: false, duration: 500});
-	$("#content").delay(875).animate({"top": "0px"}, 500);
+	var pages = ["Home", "About", "Education", "Skills", "Projects", "Contact"];
+	var navEleHeight = 200/pages.length;
+	$("#navList").css("height", navEleHeight*pages.length);
+	for(x=0; x<pages.length; x++){
+		var code = $("<li></li>").attr("id", pages[x]).html(pages[x]);
+		$(code).css("height", navEleHeight);
+		$(code).addClass("hvr-back-pulse");
+		$("#navList").append(code);
+		if(x==0)
+			$("#"+pages[x]).addClass("hvr-bubble-right")
+	}
 
-	$("#side").delay(1000).fadeIn({queue: false, duration: 500});
-	$("#side").delay(1000).animate({"left": "0px"}, 500);
+	var active = "#Home";
+	active = switchTo(active);
+	
+	$(".hvr-back-pulse").each(function(){
+		var id = $(this).attr("id");
+		$("#"+id).click(function(){
+			if(("#"+id)!=active){
+				console.log(id);
+				$(active).removeClass("hvr-bubble-right");
+				$(active+"Page").fadeOut({queue: false, duration: 50});
+				active = switchTo("#"+id);
+			}
+		});
+	});
+
+	function switchTo(to){
+		$(to+"Page").delay(1000).fadeIn({queue: false, duration: 500});
+		$(to).addClass("hvr-bubble-right");
+		return to;
+	}
 
 	$("#welcome").typed({
 		strings: ["Hi there, I'm Adam."],
-		contentType: 'html',
+		contentType: "html",
 		startDelay: 2000,
 		typeSpeed: 100
 	});
-
-	$("#homeButton").addClass("hvr-bubble-right");
-
-	var buttonArr = ["#homeButton", "#educationButton", "#skillsButton", "#projectsButton", "#contactButton"];
-	var contentArr = ["#home", "#education", "#skills", "#projects", "#contact"];
-	
-	$("#educationButton").click(function(){
-		current(buttonArr, contentArr);
-		$("#educationButton").addClass("hvr-bubble-right");
-		$("#education").addClass("active");
-		$("#education").fadeIn({queue: false, duration: 500});
-	});
-
-	$("#projectsButton").click(function(){
-		current(buttonArr, contentArr);
-		$("#projectsButton").addClass("hvr-bubble-right");
-		$("#projects").addClass("active");
-		$("#projects").delay(500).fadeIn({queue: false, duration: 500});
-	});
-
-	$("#skillsButton").click(function(){
-		current(buttonArr, contentArr);
-		$("#skillsButton").addClass("hvr-bubble-right");
-		$("#skills").addClass("active");
-		$("#skills").delay(500).fadeIn({queue: false, duration: 500});
-	});
-
-	$("#contactButton").click(function(){
-		current(buttonArr, contentArr);
-		$("#contactButton").addClass("hvr-bubble-right");
-		$("#contact").addClass("active");
-		$("#contact").delay(500).fadeIn({queue: false, duration: 500});
-	});
-
-	$("#homeButton").click(function(){
-		current(buttonArr, contentArr);
-		$("#homeButton").addClass("hvr-bubble-right");
-		$("#home").addClass("active");
-		$("#home").delay(500).fadeIn({queue: false, duration: 500});
-	});
-
-	function current(buttonArr, contentArr){
-		for(var x=0; x<buttonArr.length; x++){
-			if($(buttonArr[x]).hasClass("hvr-bubble-right")){
-				$(buttonArr[x]).removeClass("hvr-bubble-right");
-			}
-		}
-		for(var y=0; y<contentArr.length; y++){
-			if($(contentArr[y]).hasClass("active")){
-				$(contentArr[y]).removeClass("active");
-				$(contentArr[y]).fadeOut({queue: false, duration: 100});
-			}
-		}
-	}
 });
