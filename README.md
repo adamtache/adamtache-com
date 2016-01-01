551b873cd663daef34c4fe85b0965070fbcc2b6a
Adam Tache <at201@duke.edu>
2016-01-01 01:55:22 -0500

    Updated content

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ Adam Tache's personal website. Made from scratch using Javascript, jQuery, HTML,
 
 HTML file: index.html, JS file: custom.js, CSS file: custom.css
 
-Type animation uses typed.js and hover animations using hover.js
+Type animation uses typed.js and hover animations using hover.css
--- js/custom.js
+++ js/custom.js
@@ -1,61 +1,71 @@
 $(document).ready(function(){
 	$("#background").fadeTo(2000, 0.5);
-	var topSlides = ["#nav","#title","#content"];
-	var leftSlides = ["#side"];
-	var topLength = topSlides.length;
-	var leftLength = leftSlides.length;
-	var del = 500;
+	var topSlides = ["#nav","#title","#content"]; // Elements to slide in from top at page load
+	var leftSlides = ["#side"]; // Elements to slide in from left at page load
+	var del = 500; // initial delay in miliseconds
 	var x;
-	for(x=0; x<topLength; x++){
-		topSlide(topSlides[x], del+=125);
+	for(x=0; x<topSlides.length; x++){
+		topSlide(topSlides[x], del+=125); // Slide in elements with delay of del
 	}
-	for(x=0; x<leftLength; x++){
-		leftSlide(leftSlides[x], del+=125);
+	for(x=0; x<leftSlides.length; x++){
+		leftSlide(leftSlides[x], del+=125); // Slide in elements with delay of del
 	}
 
+	//Pre-condition: hidden element
+	//Post-condition: element faded in then slid into position from top
 	function topSlide(ele, del){
 		$(ele).delay(del).fadeIn({queue: false, duration: 500});
 		$(ele).delay(del).animate({"top": "0px"}, 500);
 	}
 
+	//Pre-condition: hidden element
+	//Post-condition: element faded in then slid into position from left
 	function leftSlide(ele, del){
 		$(ele).delay(del).fadeIn({queue: false, duration: 500});
 		$(ele).delay(del).animate({"left": "0px"}, 500);
 	}
 
-	var pages = ["Home", "About", "Education", "Skills", "Projects", "Contact"];
-	var navEleHeight = 200/pages.length;
+	var pages = ["Home", "About", "Education", "Skills", "Projects", "Contact"]; // Navigation pages
+	for(x=0; x<pages.length; x++){
+		$("#"+pages[x]+"Page").css("display", "none").css("margin-bottom", "10px");
+	}
+	var navEleHeight = 200/pages.length; // Height of each nav button
 	$("#navList").css("height", navEleHeight*pages.length);
 	for(x=0; x<pages.length; x++){
 		var code = $("<li></li>").attr("id", pages[x]).html(pages[x]);
 		$(code).css("height", navEleHeight);
 		$(code).addClass("hvr-back-pulse");
-		$("#navList").append(code);
+		$("#navList").append(code); // Adds each navigation page as li element to ul#navList
 		if(x==0)
-			$("#"+pages[x]).addClass("hvr-bubble-right")
+			$("#"+pages[x]).addClass("hvr-bubble-right") // Adds arrow to current page
 	}
 
-	var active = "#Home";
-	active = switchTo(active);
-	
-	$(".hvr-back-pulse").each(function(){
-		var id = $(this).attr("id");
-		$("#"+id).click(function(){
-			if(("#"+id)!=active){
-				console.log(id);
-				$(active).removeClass("hvr-bubble-right");
-				$(active+"Page").fadeOut({queue: false, duration: 50});
-				active = switchTo("#"+id);
-			}
-		});
-	});
+	var active = "#Home"; // Currently displayed page
+	active = switchTo(active); // Display initial page
 
+	//Pre-condition: "to" is nav element clicked with corresponding hidden page
+	//Post-condition: page faded in and arrow added to "to"
 	function switchTo(to){
 		$(to+"Page").delay(1000).fadeIn({queue: false, duration: 500});
 		$(to).addClass("hvr-bubble-right");
 		return to;
 	}
+	
+	// Detects click for each nav element and adjusts page content
+	$(".hvr-back-pulse").each(function(){
+		var id = $(this).attr("id");
+		// Adds click to each nav element
+		$("#"+id).click(function(){
+			if(("#"+id)!=active){ // Only adjust content if different page than currently active one clicked
+				console.log(id);
+				$(active).removeClass("hvr-bubble-right");
+				$(active+"Page").css("display","none");
+				active = switchTo("#"+id);
+			}
+		});
+	});
 
+	// Controls typing script from typed.js
 	$("#welcome").typed({
 		strings: ["Hi there, I'm Adam."],
 		contentType: "html",
